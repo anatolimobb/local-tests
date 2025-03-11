6ae00d266e6aad5e54af966b45ab782e23c702b0
Anatoli <155424115+anatolimobb@users.noreply.github.com>
2025-03-11 14:59:52 +0200

    Update xss-report.js

--- xss-report.js
+++ xss-report.js
@@ -1,4 +1,5 @@
 // This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
+// test
 
 var urlParams = new URLSearchParams(window.location.search)
 var username = urlParams.get('username')
