356a69b45bedc574ea698a9ae8158916db15c20f
Anatoli <155424115+anatolimobb@users.noreply.github.com>
2025-03-11 18:18:45 +0200

    Delete xss-report.js

--- xss-report.js
+++
@@ -1,6 +0,0 @@
-// This is an intentionaly vulnerable Javascript file used for testing, do not use or deploy it anywhere.
-
-var urlParams = new URLSearchParams(window.location.search)
-var username = urlParams.get('username')
-
-document.write('Hell11111o to you!!!!!!!, ' + username + '!!!')
